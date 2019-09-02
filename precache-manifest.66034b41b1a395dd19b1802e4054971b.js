2f4f80c179179e43b1d52fba4a293f1f11446d09
Benjamin Lee <bnllee@ucdavis.edu>
2019-09-01 23:50:01 -0700

    Updates

---
+++ precache-manifest.66034b41b1a395dd19b1802e4054971b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6bde41761a42e602d75ab98d5ad752",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "5eb76b99923d34dc62ea",
+    "url": "/react-tutorial/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "854d68ec4190dad53f1b",
+    "url": "/react-tutorial/static/js/2.d7b9399b.chunk.js"
+  },
+  {
+    "revision": "5eb76b99923d34dc62ea",
+    "url": "/react-tutorial/static/js/main.528b1928.chunk.js"
+  },
+  {
+    "revision": "be44a969ea6f00258bce",
+    "url": "/react-tutorial/static/js/runtime~main.4adc765e.js"
+  }
+]);
